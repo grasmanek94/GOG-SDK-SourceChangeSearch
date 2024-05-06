e065d1e56547927bf631ea57ba19c17b8e6a2ebd
grasmanek94 <github@gz0.nl>
2024-05-07 00:03:47 +0200

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SourceChangeSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text_input = new System.Windows.Forms.RichTextBox();
+            this.results_textbox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // text_input
+            // 
+            this.text_input.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.text_input.BackColor = System.Drawing.Color.Black;
+            this.text_input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.text_input.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_input.ForeColor = System.Drawing.Color.Lime;
+            this.text_input.Location = new System.Drawing.Point(0, 0);
+            this.text_input.Name = "text_input";
+            this.text_input.Size = new System.Drawing.Size(732, 70);
+            this.text_input.TabIndex = 0;
+            this.text_input.Text = "";
+            this.text_input.TextChanged += new System.EventHandler(this.text_input_TextChanged);
+            // 
+            // results_textbox
+            // 
+            this.results_textbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.results_textbox.BackColor = System.Drawing.Color.Black;
+            this.results_textbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.results_textbox.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.results_textbox.ForeColor = System.Drawing.Color.Lime;
+            this.results_textbox.Location = new System.Drawing.Point(0, 70);
+            this.results_textbox.Name = "results_textbox";
+            this.results_textbox.ReadOnly = true;
+            this.results_textbox.Size = new System.Drawing.Size(732, 385);
+            this.results_textbox.TabIndex = 1;
+            this.results_textbox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 456);
+            this.Controls.Add(this.results_textbox);
+            this.Controls.Add(this.text_input);
+            this.MinimumSize = new System.Drawing.Size(282, 194);
+            this.Name = "Form1";
+            this.Text = "Source Change Searcher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox text_input;
+        private System.Windows.Forms.RichTextBox results_textbox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SourceChangeSearch
+{
+    public partial class Form1 : Form
+    {
+        Thread t = null;
+        volatile bool running = true;
+        string text = string.Empty;
+        volatile bool updated = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            t = new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                while(running)
+                {
+                    if(updated)
+                    {
+                        string text_copy = "";
+                        lock (text)
+                        {
+                            text_copy = string.Copy(text);
+                        }
+
+                        updated = false;
+
+                        if (text != null)
+                        {
+                            Process p = new Process();
+                            // Redirect the output stream of the child process.
+                            p.StartInfo.UseShellExecute = false;
+                            p.StartInfo.RedirectStandardOutput = true;
+                            p.StartInfo.CreateNoWindow = true;
+
+                            p.StartInfo.FileName = "git";
+                            p.StartInfo.Arguments = "log -S \"" + text + "\"";
+                            p.Start();
+                            string output = p.StandardOutput.ReadToEnd();
+                            p.WaitForExit();
+
+                            string filteredLines = string.Join("\n", output.Split('\n').Where(l => l.Contains("GOG Galaxy SDK")).ToArray());
+                            results_textbox.BeginInvoke(new Action(() =>
+                            {
+                                    results_textbox.Text = filteredLines;
+                            }));
+                        }
+                    }
+                }
+            });
+
+            t.Start();
+        }
+
+        private void text_input_TextChanged(object sender, EventArgs e)
+        {
+            lock (text)
+            {
+                text = text_input.Text;
+                updated = true;
+            }        
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            running = false;
+        }
+    }
+}
